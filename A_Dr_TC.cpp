81d73291b667b97ef080754a815355b35e65eeae
SayedHasanSami2 <samihasan99655@gmail.com>
2025-09-02 13:49:38 +0600

    DR. TC added

---
+++ A_Dr_TC.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int count =0;
+        for(char c : s)
+        {
+            if(c == '1')
+                count++;
+        }
+
+        int ans =0;
+        for(char c : s)
+        {
+            if(c == '0')
+                ans+= count+1;
+            else
+                ans+= count -1;
+
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
