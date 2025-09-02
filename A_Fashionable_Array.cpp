65e01c2d52971526025e10087ba9ed34b8b9f9a3
SayedHasanSami2 <samihasan99655@gmail.com>
2025-09-02 13:35:26 +0600

    new file added

---
+++ A_Fashionable_Array.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        sort(arr, arr + n);
+        int mn = arr[0];
+        int mx = arr[n - 1];
+
+        if (mn % 2 == mx % 2) // 1 1 2 3 4 5 6 7 8
+            cout << 0 << endl;
+        else
+        {
+            int i = 0, count = 0;
+            while (arr[i] % 2 != mx % 2)
+            {
+
+                i++;
+                count++;
+            }
+            int j = n - 1, count1 = 0;
+            while (arr[j] % 2 != mn % 2)
+            {
+
+                j--;
+                count1++;
+            }
+            cout << min(count, count1) << endl;
+        }
+    }
+    return 0;
+}
